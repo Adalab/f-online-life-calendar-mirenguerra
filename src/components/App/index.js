d2de3b2de9255ab7b2dcd496228ba58cac777c0e
mirenguerra <miren.guerra.sanz@gmail.com>
2019-06-24 17:14:09 +0200

    Get date

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,12 +5,27 @@ import Editor from "../Editor";
 import { Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      date:'',
+    }
+    this.handleChangeDate=this.handleChangeDate.bind(this);
+  }
+
+  handleChangeDate(event){
+    const dateValue = event.currentTarget.value;
+    this.setState({
+      date: dateValue,
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <Switch>
           <Route exact path="/" render={() => <Calendar />} />
-          <Route path="/editor" render={() => <Editor />} />
+          <Route path="/editor" render={() => <Editor handleChangeDate={this.handleChangeDate} />} />
         </Switch>
       </div>
     );
