c517f3d46744c50c54eade5b69049f96be8192fe
mirenguerra <miren.guerra.sanz@gmail.com>
2019-06-27 13:02:14 +0200

    Create docs

---
+++ docs/precache-manifest.1d871230ec4204138d6acf5e3398f7ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad88b344295d26006b58f169e6b6f0fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ff449fff03d03b1d16c",
+    "url": "./static/css/main.9a60960a.chunk.css"
+  },
+  {
+    "revision": "5f64473fffea26737c81",
+    "url": "./static/js/2.efd3be52.chunk.js"
+  },
+  {
+    "revision": "4ff449fff03d03b1d16c",
+    "url": "./static/js/main.c7770610.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ea53c2f0d5e16bdd49b3771a2a00516e",
+    "url": "./static/media/smilies.ea53c2f0.jpg"
+  }
+]);
